d03fdd72089b2b8ef3bd681c390c95eff574d294
raggarwal <raggarwal@tavisca.in>
2019-12-05 18:56:12 +0530

    Implemented data layer for Inventory Service

---
+++ IMS.DataLayer/Db/InventoryDbContext.cs
@@ -0,0 +1,90 @@
+﻿using IMS.DataLayer.Interfaces;
+using IMS.Entities;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace IMS.DataLayer.Db
+{
+    public class InventoryDbContext : IInventoryDbContext
+    {
+        private IDbConnectionProvider _dbConnectionProvider;
+
+        public InventoryDbContext(IDbConnectionProvider dbConnectionProvider)
+        {
+            _dbConnectionProvider = dbConnectionProvider;
+        }
+
+        public ShelfItemResponse GetShelfItemsByShelfId(int shelfId)
+        {
+            MySqlDataReader reader = null;
+            ShelfItemResponse shelfItemResponse = new ShelfItemResponse();
+            List<ItemQuantityMapping> itemQuantityMappingList = new List<ItemQuantityMapping>();
+            using (var connection = _dbConnectionProvider.GetConnection(Databases.IMS))
+            {
+                try
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "spGetShelfItemsByShelfId";
+                    command.Parameters.AddWithValue("@shelfId", shelfId);
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        itemQuantityMappingList.Add(new ItemQuantityMapping()
+                        {
+                            Item = new Item()
+                            {
+                                Id = (int)reader["ItemId"],
+                                Name = (string)reader["ItemName"],
+                                MaxLimit = (int)reader["ItemMaximumLimit"],
+                                isActive = (bool)reader["ItemState"]
+                            },
+                            Quantity = (int)reader["ItemQuantityAtShelf"]
+                        });
+                    }
+                    Shelf shelf = GetShelfById(1);
+                    shelfItemResponse.itemQuantityMapping = itemQuantityMappingList;
+                    shelfItemResponse.shelf = shelf;
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                return shelfItemResponse;
+            }
+        }
+        public Shelf GetShelfById(int shelfId)
+        {
+            MySqlDataReader reader = null;
+            Shelf shelf = new Shelf();
+            using (var connection = _dbConnectionProvider.GetConnection(Databases.IMS))
+            {
+                try
+                {
+                    connection.Open();
+                    var command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "spGetShelfById";
+                    command.Parameters.AddWithValue("@shelfId", shelfId);
+                    reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        shelf.Id = (int)reader["Id"];
+                        shelf.Name = (string)reader["Name"];
+                        shelf.IsActive = (bool)reader["IsActive"];
+                        shelf.Code = (string)reader["Code"];
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+            }
+            return shelf;
+        }
+    }
+}
---
+++ IMS.Entities/ShelfItemResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class ShelfItemResponse : Response
+    {
+        public Shelf shelf { get; set; }
+        public List<ItemQuantityMapping> itemQuantityMapping { get; set; }
+    }
+}
