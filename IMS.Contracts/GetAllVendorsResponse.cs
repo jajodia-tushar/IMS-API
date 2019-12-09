d18a434f18f19ebe6418348df6c6c3eb8272a16a
vvinod <vvinod@tavisca.in>
2019-12-09 11:15:02 +0530

    Added response and model classes.

---
+++ IMS.Contracts/GetAllVendorsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Contracts
+{
+    public class GetAllVendorsResponse : Response
+    {
+        public List<Vendor> Vendors { get; set; }
+    }
+}
---
+++ IMS.Entities/GetAllVendorsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class GetAllVendorsResponse : Response
+    {
+        public List<Vendor> Vendors { get; set; }
+    }
+}
