a118e6e66e41c211e2d16e792e787cc72f40c248
vvinod <vvinod@tavisca.in>
2019-12-11 14:23:00 +0530

    Added controller method and translator class.

--- IMS-API/Controllers/TransferController.cs
+++ IMS-API/Controllers/TransferController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IMS.Contracts;
+using IMS.Core;
+using IMS.Core.Translators;
 using IMS.Entities.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -18,15 +20,39 @@ namespace IMS_API.Controllers
         {
             this._transferService = transferService;
         }
+        /// <summary>
+        /// Transfer items from warehouse to shelf
+        /// </summary>
+        /// <param name="transferToShelvesRequest">The list of items and shelves to which transfer has to be made</param>
+        /// <returns>Status</returns>
+        /// <response code="200">Returns Success if tranfser is successfull else returns status failure</response>
         // Patch: api/TransferToShelves
         [HttpPatch("TransferToShelves")]
-        public Response TransferToShelf([FromBody] TransferToShelvesRequest transferToShelvesRequest)
+        public async Task<Response> TransferToShelf([FromBody] TransferToShelvesRequest transferToShelvesRequest)
         {
-            Response response = new Response();
-            return response;
+            Response transferToShelfResponse = null;
+            try
+            {
+                IMS.Entities.TransferToShelvesRequest entityTransferToShelvesRequest = TransferTranslator.ToEntitiesObject(transferToShelvesRequest);
+                IMS.Entities.Response entityTransferToShelfResponse = await _transferService.TransferToShelves(entityTransferToShelvesRequest);
+                transferToShelfResponse = TransferTranslator.ToDataContractsObject(entityTransferToShelfResponse);
+            }
+            catch
+            {
+                transferToShelfResponse = new IMS.Contracts.GetEmployeeResponse()
+                {
+                    Status = Status.Failure,
+                    Error = new Error()
+                    {
+                        ErrorCode = Constants.ErrorCodes.ServerError,
+                        ErrorMessage = Constants.ErrorMessages.ServerError
+                    }
+                };
+            }
+            return transferToShelfResponse;
         }
 
         // Patch: api/TransferToWarehouse
-
+   
     }
 }
---
+++ IMS.Core/Translators/TransferTranslator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMS.Contracts;
+using IMS.Entities;
+
+namespace IMS.Core.Translators
+{
+    public class TransferTranslator
+    {
+        public static Entities.TransferToShelvesRequest ToEntitiesObject(Contracts.TransferToShelvesRequest transferToShelvesRequest)
+        {
+            return new Entities.TransferToShelvesRequest()
+            {
+
+            };
+        }
+
+        public static Contracts.Response ToDataContractsObject(Entities.Response entityTransferToShelfResponse)
+        {
+            return new Contracts.Response()
+            {
+
+            };
+        }
+    }
+}
