10183e9ff44529b8d581373ca41f0dee472c7676
tavisca-vreddy <51921004+tavisca-vreddy@users.noreply.github.com>
2019-12-14 13:31:22 +0530

    applied readonly property for TotalPrice and FinalAmount

--- IMS.Contracts/VendorOrderDetails.cs
+++ IMS.Contracts/VendorOrderDetails.cs
@@ -10,11 +10,25 @@ namespace IMS.Contracts
         public bool IsApproved { get; set; }
         public string RecievedBy { get; set; }
         public string SubmittedTo { get; set; }
-        public double FinalAmount { get; set; }
+        
         public double TaxableAmount { get; set; }
         public string InvoiceNumber { get; set; }
         public string InvoiceImageUrl { get; set; }
         public DateTime Date { get; set; }
         public List<ItemQuantityPriceMapping> OrderItemDetails { get; set; }
+        public double FinalAmount
+        {
+            get
+            {
+                double totalAmount = 0;
+                if (this.OrderItemDetails != null && this.OrderItemDetails.Count != 0)
+                {
+                    foreach (ItemQuantityPriceMapping itemQtyPrice in this.OrderItemDetails)
+                        totalAmount += itemQtyPrice.TotalPrice;
+                    totalAmount += this.TaxableAmount;
+                }
+                return Math.Round(totalAmount, 2);
+            }
+        }
     }
 }
