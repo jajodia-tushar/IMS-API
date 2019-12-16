0ee16c0236f90828c0b41ef883aa14effdb3afe0
tkatyal <tkatyal@tavisca.in>
2019-12-16 15:50:12 +0530

    Made Model Class

---
+++ IMS.Entities/EmployeeRecentOrder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class EmployeeRecentOrder
+    {
+        public Employee Employee { get; set; }
+        public EmployeeOrderDetails EmployeeOrder { get; set; }
+    }
+}
---
+++ IMS.Entities/EmployeeRecentOrderResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class EmployeeRecentOrderResponse : Response
+    {
+        public List<EmployeeRecentOrder> EmployeeRecentOrders { get; set; }
+    }
+}
