2b9cb4c3070ef828d6c35f642a91d3516c5e731d
tkatyal <tkatyal@tavisca.in>
2019-12-16 15:56:55 +0530

    Implemented Translator

--- IMS.Core/Translators/EmployeeOrderTranslator.cs
+++ IMS.Core/Translators/EmployeeOrderTranslator.cs
@@ -10,7 +10,41 @@ namespace IMS.Core.Translators
     {
         public static Contracts.EmployeeRecentOrderResponse ToDataContractsObject(Entities.EmployeeRecentOrderResponse doEmployeeRecentOrderResponse)
         {
-            throw new NotImplementedException();
+            Contracts.EmployeeRecentOrderResponse dtoEmployeeRecentOrderResponse = new Contracts.EmployeeRecentOrderResponse();
+            dtoEmployeeRecentOrderResponse.Status = Contracts.Status.Failure;
+            if (doEmployeeRecentOrderResponse.Status == Entities.Status.Success)
+            {
+                dtoEmployeeRecentOrderResponse.Status = Contracts.Status.Success;
+                dtoEmployeeRecentOrderResponse.Error = null;
+                dtoEmployeeRecentOrderResponse.EmployeeRecentOrders = ToDataContractsObject(doEmployeeRecentOrderResponse.EmployeeRecentOrders);
+            }
+            else
+            {
+                dtoEmployeeRecentOrderResponse.Status = Contracts.Status.Failure;
+                dtoEmployeeRecentOrderResponse.Error = Translator.ToDataContractsObject(doEmployeeRecentOrderResponse.Error);
+            }
+
+            return dtoEmployeeRecentOrderResponse;
         }
+
+        public static List<Contracts.EmployeeRecentOrder> ToDataContractsObject(List<Entities.EmployeeRecentOrder> employeeRecentOrders)
+        {
+            List<Contracts.EmployeeRecentOrder> dtoEmployeeRecentOrder = new List<Contracts.EmployeeRecentOrder>();
+            foreach (var recentOrder in employeeRecentOrders)
+            {
+                dtoEmployeeRecentOrder.Add(ToDataContractsObject(recentOrder));
+            }
+
+            return dtoEmployeeRecentOrder;
+        }
+
+        public static Contracts.EmployeeRecentOrder ToDataContractsObject(Entities.EmployeeRecentOrder recentOrder)
+        {
+            Contracts.EmployeeRecentOrder dtoEmployeeRecentOrder = new Contracts.EmployeeRecentOrder();
+            dtoEmployeeRecentOrder.Employee = EmployeeTranslator.ToDataContractsObject(recentOrder.Employee);
+            dtoEmployeeRecentOrder.EmployeeOrder = ToDataContractsObject(recentOrder.EmployeeOrder);
+            return dtoEmployeeRecentOrder;
+        }
+
     }
 }
