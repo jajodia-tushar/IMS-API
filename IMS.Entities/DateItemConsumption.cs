555abed3290d4441ba9813c8e4da433f54f5fcc3
vvinod <vvinod@tavisca.in>
2019-12-20 19:12:12 +0530

    Contracts for total item consumption in a given range.

---
+++ IMS.Entities/DateItemConsumption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class DateItemConsumption
+    {
+        public string Date { get; set; }
+        public int CountOfItems { get; set; }
+    }
+}
---
+++ IMS.Entities/ItemsConsumptionCountResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class ItemsConsumptionCountResponse:Response
+    {
+        public List<DateItemConsumption> DateItemConsumptionList { get; set; }
+    }
+}
