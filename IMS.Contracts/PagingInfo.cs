6ffed2189d86eb5f95c18f1d3a6be43f0942c192
tkatyal <tkatyal@tavisca.in>
2019-12-26 13:46:30 +0530

    added model class

---
+++ IMS.Contracts/PagingInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Contracts
+{
+    public class PagingInfo
+    {
+        public int CurrentPageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+        public int TotalEntries { get; set; }
+    }
+}
