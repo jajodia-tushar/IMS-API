1378da06ea5630b454d787aeec47c6dbce9732b2
tkatyal <tkatyal@tavisca.in>
2019-12-26 13:48:19 +0530

    added object of paging info class in recent order response

--- IMS.Entities/EmployeeRecentOrderResponse.cs
+++ IMS.Entities/EmployeeRecentOrderResponse.cs
@@ -7,5 +7,6 @@ namespace IMS.Entities
     public class EmployeeRecentOrderResponse : Response
     {
         public List<EmployeeRecentOrder> EmployeeRecentOrders { get; set; }
+        public PagingInfo PagingInfo { get; set; }
     }
 }
