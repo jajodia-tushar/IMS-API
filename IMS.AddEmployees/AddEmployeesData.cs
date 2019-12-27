9249c4b8c050482f4b1d17aea9034f084555102c
raggarwal <raggarwal@tavisca.in>
2019-12-27 20:11:17 +0530

    added logging

--- IMS.AddEmployees/AddEmployeesData.cs
+++ IMS.AddEmployees/AddEmployeesData.cs
@@ -1,4 +1,5 @@
-﻿using IMS.DataLayer.Interfaces;
+﻿using IMS.Core;
+using IMS.DataLayer.Interfaces;
 using IMS.Entities;
 using IMS.Logging;
 using Microsoft.Extensions.Configuration;
@@ -24,6 +25,7 @@ namespace IMS.AddEmployees
         }
         public void AddEmployeesFromCsvFile()
         {
+            Response employeesAddResponse = new Response();
             var employeesNotAddedFileLocation = @"C:\IMSEmployeesData";
             try
             {
@@ -33,12 +35,25 @@ namespace IMS.AddEmployees
                 if (!Directory.Exists(employeesNotAddedFileLocation))
                     Directory.CreateDirectory(employeesNotAddedFileLocation);
                 File.WriteAllLines(Path.Combine(employeesNotAddedFileLocation, "IMSEmployeesNotAdded.txt"), employeesNotAdded);
+                if (employeesNotAdded.Count > 0)
+                    employeesAddResponse.Status = Status.Failure;
+                else
+                    employeesAddResponse.Status = Status.Success;
             }
             catch (Exception exception)
             {
-                Console.WriteLine("Status: "+Status.Failure);
+                employeesAddResponse.Status = Status.Failure;
+                employeesAddResponse.Error = Utility.ErrorGenerator(Constants.ErrorCodes.ServerError,Constants.ErrorMessages.ServerError);
+                Console.WriteLine("Status: "+employeesAddResponse.Status);
                 Console.WriteLine("Sorry, Internal Server Error");
-                new Task(() => { _logger.LogException(exception, "AddEmployeesFromCsvFile", Severity.High, null, Status.Failure); }).Start();
+                new Task(() => { _logger.LogException(exception, "AddEmployeesFromCsvFile", Severity.High, null,employeesAddResponse); }).Start();
+            }
+            finally
+            {
+                Severity severity = Severity.No;
+                if (employeesAddResponse.Status == Status.Failure)
+                    severity = Severity.High;
+                new Task(() => { _logger.Log(null, employeesAddResponse, "AddEmployeesFromCsvFile", employeesAddResponse.Status, severity, -1); }).Start();
             }
         }
         public List<Employee> ReadEmployeesDataFromCsv(string[] employeesData)
