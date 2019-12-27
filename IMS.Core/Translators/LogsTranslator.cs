aa3600e94db002d443d748638a82ab065ac0573c
tkatyal <tkatyal@tavisca.in>
2019-12-27 15:51:54 +0530

    implemented Translator

--- IMS.Core/Translators/LogsTranslator.cs
+++ IMS.Core/Translators/LogsTranslator.cs
@@ -10,7 +10,49 @@ namespace IMS.Core.Translators
     {
         public static Contracts.LogsResponse ToDataContractsObject(Entities.LogsResponse doLogsResponse)
         {
-            throw new NotImplementedException();
+            if(doLogsResponse!=null)
+            {
+                return new Contracts.LogsResponse()
+                {
+                    Status = doLogsResponse.Status == Entities.Status.Success ? Contracts.Status.Success : Contracts.Status.Failure,
+                    Error  = doLogsResponse.Error == null?null:Translator.ToDataContractsObject(doLogsResponse.Error),
+                    LogsRecords = doLogsResponse.LogsRecords==null?null :ToDataContractsObject(doLogsResponse.LogsRecords)
+                };
+            }
+            return null;
+        }
+
+        private static List<Contracts.Logs> ToDataContractsObject(List<Entities.Logs> doLogsRecords)
+        {
+            List<Contracts.Logs> dtoLogRecords = new List<Contracts.Logs>();
+            if(doLogsRecords!=null)
+            {
+                foreach(var log in doLogsRecords)
+                {
+                    dtoLogRecords.Add(ToDataContractsObject(log));
+                }
+                return dtoLogRecords;
+            }
+            return dtoLogRecords;
+        }
+
+        private static Contracts.Logs ToDataContractsObject(Entities.Logs doLog)
+        {
+            Contracts.Logs dtoLog = new Contracts.Logs();
+            if(doLog!=null)
+            {
+                dtoLog.Id = doLog.Id;
+                dtoLog.UserId = doLog.UserId;
+                dtoLog.CallType = doLog.CallType;
+                dtoLog.Request = doLog.Request;
+                dtoLog.Response = doLog.Response;
+                dtoLog.Severity = doLog.Severity;
+                dtoLog.Status = doLog.Status;
+                dtoLog.DateTime = doLog.DateTime;
+
+                return dtoLog;
+            }
+            return dtoLog;
         }
     }
 }
