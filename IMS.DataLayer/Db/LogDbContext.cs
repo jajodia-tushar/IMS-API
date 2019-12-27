2fac17f3789056537ccc69a0174f5a2f53390ee7
tkatyal <tkatyal@tavisca.in>
2019-12-27 15:00:14 +0530

    Implemented get logs method

--- IMS.DataLayer/Db/LogDbContext.cs
+++ IMS.DataLayer/Db/LogDbContext.cs
@@ -20,7 +20,45 @@ namespace IMS.DataLayer.Db
 
         public async Task<List<Logs>> GetLogs()
         {
-            throw new NotImplementedException();
+            List<Logs> listOfLog = new List<Logs>();
+            MySqlDataReader reader = null;
+            try
+            {
+                using (var connection = _dbConnectionProvider.GetConnection(Databases.LOGGING))
+                {
+
+                    connection.Open();
+
+                    var command = connection.CreateCommand();
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "spGetLogs";
+
+                    reader = command.ExecuteReader();
+                    while(reader.Read())
+                    {
+                        Logs logs = new Logs()
+                        {
+                            Id = (int)reader["Id"],
+                            UserId=(int)reader["UserId"],
+                            CallType=(string)reader["CallType"],
+                            Request = (string)reader["Request"],
+                            Response=(string)reader["Response"],
+                            Severity =(string)reader["Severity"],
+                            Status =(string)reader["Status"],
+                            DateTime=(DateTime)reader["Timestamp"]
+
+                        };
+                        listOfLog.Add(logs);
+                    }
+                    reader.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            return listOfLog;
         }
 
         public void Log(int userId, string status, string callType, string severity, string request, string response)
