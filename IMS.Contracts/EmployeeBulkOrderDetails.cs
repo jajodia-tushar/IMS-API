3619622943d7321b5bc93ed6f70bc56b891acbaf
tavisca-asingla <asingla@tavisca.in>
2020-01-20 13:30:33 +0530

    Removed Custom JsonConvertor

--- IMS.Contracts/EmployeeBulkOrderDetails.cs
+++ IMS.Contracts/EmployeeBulkOrderDetails.cs
@@ -1,5 +1,6 @@
 ﻿using IMS.Contracts.Convertors;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,7 +11,7 @@ namespace IMS.Contracts
     {
         public DateTime CreatedOn { get; set; }
         public DateTime RequirementDate { get; set; }
-        [JsonConverter(typeof(BulkOrderRequestStatusConvertor))]
+        [JsonConverter(typeof(StringEnumConverter))]
         public BulkOrderRequestStatus BulkOrderRequestStatus { get; set; }
         public string ReasonForRequirement { get; set; }
         public List<ItemQuantityMapping> EmployeeItemsQuantityList { get; set; }
