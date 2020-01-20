005418e45e0d25b992f1be528855f7960cfa38dc
Tushar Jajodia <tjajodia@tavisca.com>
2020-01-20 15:59:51 +0530

    Renamed startDate and endDate

--- IMS.Entities/Interfaces/IReportsService.cs
+++ IMS.Entities/Interfaces/IReportsService.cs
@@ -14,7 +14,7 @@ namespace IMS.Entities.Interfaces
         Task<StockStatusResponse> GetStockStatus(int pageNumber, int pageSize, string itemName);
         Task<ItemsAvailabilityResponse> GetItemsAvailability(string locationName, string locationCode, string colour,int pageNumber, int pageSize);
 
-        Task<DateWiseItemsConsumption> GetItemConsumptionReports(string startDate, string endDate,int itemId);
+        Task<DateWiseItemsConsumption> GetItemConsumptionReports(string fromDate, string toDate,int itemId);
 
     }
 }
