c6aabc0e59e25635209624a46d39c4ddcab57126
Tushar Jajodia <tjajodia@tavisca.com>
2020-01-20 15:17:23 +0530

    Added Controller Services and Data layer

---
+++ IMS.Contracts/DateWiseItemsConsumption.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Contracts
+{
+    public class DateWiseItemsConsumption : Response
+    {
+        public List<DateItemsMapping> DateItemMapping { get; set; }
+    }
+}
