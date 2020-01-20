0037f22164a8c23385b539c9c4af8fbb3a2fe629
tavisca-vreddy <51921004+tavisca-vreddy@users.noreply.github.com>
2020-01-20 10:12:00 +0530

    created seperate method for token validation in utility class

---
+++ IMS.Entities/RequestData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMS.Entities
+{
+    public class RequestData
+    {
+        public bool HasValidToken { get; set; }
+        public User User { get; set; }
+    }
+}
