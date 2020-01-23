4ece48e825ae57de555bdacd9247f58577d6ebf9
tavisca-ryadav <tavisca-ryadav@tavisca.com>
2020-01-23 15:20:15 +0530

    Controller and translator added

--- IMS-API/Controllers/NotificationController.cs
+++ IMS-API/Controllers/NotificationController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IMS.Contracts;
+using IMS.Core;
+using IMS.Core.Translators;
+using IMS.Entities.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,41 +15,30 @@ namespace IMS_API.Controllers
     [ApiController]
     public class NotificationController : ControllerBase
     {
+        private IAdminNotificationService _adminNotificationService;
+        public NotificationController(IAdminNotificationService adminNotificationService)
+        {
+            _adminNotificationService = adminNotificationService;
+        }
         // GET: api/Notification
         [HttpGet]
         public async Task<NotificationResponse> Get()
         {
             var notificationResponse = new NotificationResponse();
-            var notification1 = new Notification()
+            try
             {
-                RequestType = RequestType.VendorOrder,
-                RequestStatus = RequestStatus.Edited,
-                RequestId = 234,
-                RequestedBy = "Rajat Yadav",
-                LastModified = DateTime.Now
-            };
-            var notification2 = new Notification()
+                var doNotificationResponse = await _adminNotificationService.GetAdminNotifications();
+                notificationResponse = NotificationTranslator.ToDataContractObject(doNotificationResponse);
+            }
+            catch(Exception exception)
             {
-                RequestType = RequestType.BulkOrder,
-                RequestStatus = RequestStatus.Pending,
-                RequestId = 267,
-                RequestedBy = "Ebran Khan",
-                LastModified = DateTime.Now
-            };
-            var notification3 = new Notification()
-            {
-                RequestType = RequestType.UserModification,
-                RequestStatus = RequestStatus.Approved,
-                RequestId = 132,
-                RequestedBy = "Aniket Singla",
-                LastModified = DateTime.Now
-            };
-            var notificationList = new List<Notification>();
-            notificationList.Add(notification1);
-            notificationList.Add(notification2);
-            notificationList.Add(notification3);
-            notificationResponse.Notifications = notificationList;
-            notificationResponse.Status = Status.Success;
+                notificationResponse.Status = Status.Failure;
+                notificationResponse.Error = new Error()
+                {
+                    ErrorCode = Constants.ErrorCodes.ServerError,
+                    ErrorMessage = Constants.ErrorMessages.ServerError
+                };
+            }
             return notificationResponse;
         }
 
---
+++ IMS.Core/Translators/NotificationTranslator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IMS.Contracts;
+using IMS.Entities;
+
+namespace IMS.Core.Translators
+{
+    public class NotificationTranslator
+    {
+        public static Contracts.NotificationResponse ToDataContractObject(Entities.NotificationResponse doNotificationResponse)
+        {
+            try
+            {
+                var dtoNotificationResponse = new Contracts.NotificationResponse();
+                if (doNotificationResponse.Status.Equals(Entities.Status.Success))
+                {
+                    dtoNotificationResponse.Status = Contracts.Status.Success;
+                    dtoNotificationResponse.Notifications = ToDataContractObject(doNotificationResponse.Notifications);
+                }
+                else
+                {
+                    dtoNotificationResponse.Status = Contracts.Status.Failure;
+                    dtoNotificationResponse.Error = doNotificationResponse.Error == null ? null : Translator.ToDataContractsObject(doNotificationResponse.Error);
+                }
+                return dtoNotificationResponse;
+            }
+            catch(Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        private static List<Contracts.Notification> ToDataContractObject(List<Entities.Notification> notifications)
+        {
+            try
+            {
+                var dtoNotifications = new List<Contracts.Notification>();
+                foreach (var notification in notifications)
+                {
+                    dtoNotifications.Add(ToDataContractObject(notification));
+                }
+                return dtoNotifications;
+            }
+            catch(Exception exception)
+            {
+                throw exception;
+            }
+        }
+
+        private static Contracts.Notification ToDataContractObject(Entities.Notification notification)
+        {
+            try
+            {
+                return new Contracts.Notification()
+                {
+                    RequestId = notification.RequestId,
+                    RequestedBy = notification.RequestedBy,
+                    LastModified = notification.LastModified,
+                    RequestStatus = (Contracts.RequestStatus)notification.RequestStatus,
+                    RequestType = (Contracts.RequestType)notification.RequestType
+                };
+            }
+            catch(Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
