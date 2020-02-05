0d1bd68c24d2b0d9f011c7700c26ee98d52c04fa
cchowdhary <cchowdhary@tavisca.in>
2020-02-05 22:14:09 +0530

    Try 7

--- IMS.Core/services/NotificationProvider.cs
+++ IMS.Core/services/NotificationProvider.cs
@@ -27,7 +27,8 @@ namespace IMS.Core.services
             {
                 HttpClient client = new HttpClient();
                 JObject emailJson = JsonMaker(email);
-                var content = new StringContent(emailJson.ToString()+"hello"+ _configuration["docker"]+"hello", Encoding.UTF8, "application/json");
+                email.Body=email.Body + "hello" + _configuration["docker"] + "hello";
+                var content = new StringContent(emailJson.ToString(), Encoding.UTF8, "application/json");
                 var result = client.PostAsync(_configuration["baseURL"]+"/api/Notification", content).Result;
                 if (result.IsSuccessStatusCode)
                 {
